702c476b5e079ba5366ff7c2eb54ad2883d742ff
Bermuda_UT <maxisbac@gmail.com>
2015-10-26 14:21:03 +1100

    Chapter 12 Q4 Bug Fix

--- 12Chpt/4-strbsearch.c
+++ 12Chpt/4-strbsearch.c
@@ -14,17 +14,20 @@
 #include <ctype.h>
 #include <assert.h>
 
-#define MAX_WDLEN 20
+#define DEBUG     0
+#define MAX_WDLEN 25
 #define DICT_LEN  100000
 #define DICT_PATH "/usr/share/dict/british-english"
 
-char *dictsearch(char *key, char dict[DICT_LEN][MAX_WDLEN], int keylen, int dictlen);
-int streql(char *a, char *b);
+/* unsigned char to accomodate special characters */
+unsigned char *dictsearch(unsigned char *key,
+        unsigned char dict[DICT_LEN][MAX_WDLEN], int keylen, int dictlen);
+int streql(unsigned char *a, unsigned char *b);
 
 int
 main(int argc, char **argv) {
     FILE *dictpointer;
-    char myword[MAX_WDLEN], dict[DICT_LEN][MAX_WDLEN], *found;
+    unsigned char myword[MAX_WDLEN], dict[DICT_LEN][MAX_WDLEN], *found;
     int i=0;
 
     /* Get file pointer to dictionary */
@@ -43,7 +46,8 @@ main(int argc, char **argv) {
 
 
     /* Load Dictionary */
-    while(fgets(dict[i], MAX_WDLEN, dictpointer) != NULL && i < DICT_LEN) {
+    while(fgets((char*)dict[i], MAX_WDLEN, dictpointer) != NULL &&
+            i < DICT_LEN) {
         i++;
     }
     fclose(dictpointer);
@@ -52,7 +56,7 @@ main(int argc, char **argv) {
     /* Search Loop */
     printf("Search : ");
     while(scanf("%s", myword) == 1) {
-        found = dictsearch(myword, dict, strlen(myword), i);
+        found = dictsearch(myword, dict, strlen((char*)myword), i);
 
         if(streql(found, myword) == 0) {
             printf(" > Found %s\n", myword);
@@ -67,15 +71,18 @@ main(int argc, char **argv) {
 }
 
 int
-charcmp(char *a, char *b) {
+charcmp(unsigned char *a, unsigned char *b) {
     return (*a - *b);
 }
 
 int
-streql(char *a, char *b) {
+streql(unsigned char *a, unsigned char *b) {
     int i=0;
-    while(a[i] != '\n') {
-        if(charcmp(a+i, b+i) != 0) {
+#if DEBUG
+    printf("First word %s   second %s\n", a, b);
+#endif
+    while(a[i] != '\n' && b[i] != '\0') {
+        if(a[i] != b[i]) {
             return 1;
         }
         i++;
@@ -83,42 +90,79 @@ streql(char *a, char *b) {
     return 0;
 }
 
-char
-*dictsearch(char *key, char dict[DICT_LEN][MAX_WDLEN], int keylen, int dictlen) {
-    int lo, hi, depth=0, start=0, end=dictlen, mid;
+unsigned char
+*dictsearch(unsigned char *key, unsigned char dict[DICT_LEN][MAX_WDLEN],
+        int keylen, int dictlen) {
+    int lo, hi, depth=0, start=0, end=dictlen-1, mid, res;
+    lo = start;
+    hi = end;
 
     while(depth <= keylen) {
         lo = start;
         hi = end;
 
         while(lo<hi) {
+            /* find starting point of the value */
             mid = (lo+hi)/2;
-            if(charcmp(dict[mid]+depth, key+depth) < 0) {
-                /* bring up low */
+#if DEBUG
+            printf("[lo]checking %d(%c) with %d(%c)\n",
+                    key[depth], key[depth], dict[mid][depth],dict[mid][depth]);
+            printf("lo %d hi %d mid %d\n", lo, hi, mid);
+            printf(">lo %s>hi %s>mid %s\n", dict[lo], dict[hi], dict[mid]);
+#endif
+            if((res=charcmp(key+depth, dict[mid]+depth)) < 0) {
+                /* key is smaller > make hi lower */
+                hi = mid-1;
+            } else if(res > 0) {
+                /* key is greater > make lo higher */
                 lo = mid+1;
             } else {
+                /* key. make this hi */
                 hi = mid;
             }
         }
 
         start=lo;
+#if DEBUG
+        printf("--\n");
+#endif
         hi=end;
 
         while(lo<hi) {
+            /* find ending point of the value */
             mid = (lo+hi)/2;
-            if(charcmp(dict[mid]+depth, key+depth) > 0) {
-                /* bring down high */
-                hi = mid;
+#if DEBUG
+            printf("[hi]checking %d(%c) with %d(%c)\n",
+                    key[depth], key[depth], dict[mid][depth],dict[mid][depth]);
+            printf("lo %d hi %d mid %d\n", lo, hi, mid);
+            printf(">lo %s>hi %s>mid %s\n", dict[lo], dict[hi], dict[mid]);
+#endif
+            if((res=charcmp(key+depth, dict[mid]+depth)) < 0) {
+                /* key is smaller > make hi lower */
+                hi = mid-1;
+            } else if(res > 0) {
+                /* key is greater > make lo higher */
+                lo = mid+1;
             } else {
+                /* key. > make lo higher */
                 lo = mid+1;
             }
+            /*
+            int z;
+            scanf("%d",&z);
+            */
         }
 
         end=hi;
-
         depth++;
+#if DEBUG
+        printf(">> Depth++\n\n");
+#endif
     }
 
-    assert(start == end);
+#if DEBUG
+    printf("start %d end %d\n", start, end);
+#endif
+    assert(start >= end);
     return dict[start];
 }
